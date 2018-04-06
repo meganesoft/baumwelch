c542d2ee14434d9e14c21ff13a50b6a2c505dd96
otsuka <meganesoft87@gmai.com>
2018-04-06 13:12:34 +0900

    multiple repeat at position

--- markov.py
+++ markov.py
@@ -6,24 +6,33 @@ ginza = pd.read_csv('ginza_sample_keio.csv')
 
 ginza.columns = ["day","post","devies","etc","etc2","etc3","etc4","etc5"]
 
-def convert_date(day):
-	day_mod = re.sub('-','/',day)
-	day_mod = re.sub('T06|.***Z',' ',day_mod)
-	dt.datetime.strptime(day_mod,'%Y-%m-%d %H:%M:%S')
-	return day_mod
+def convert_date(days):
+	for(day) in days:
+		print("yes")
+		day = re.sub('-','/',day)
+		day = re.sub('T0*|.***Z',' ',day)
+		day = dt.datetime.strptime(day,'%Y-%m-%d %H:%M:%S')
+	return days
 
-#日付を比較して特定の間隔でデータをまとめる
+# 日付を比較して特定の間隔でデータをまとめる
 def comparison_date(column,interval):
 	base_day = column[0]
 	markov = []
 	for (index,day) in enumerate(column):
-		if day - base_day.day < interval && base_day.devies == day.devies:
+		if day - base_day.day < interval and base_day.devies == day.devies:
 		#ここに行列を作成するコードを書く
-		markov.append(day.post)
+			markov.append(day.post)
 		else:
 		#やっぱこっちに書く
-		c_markov = Counter(markov)
-		
-		base_day = day
-		markov = []	
-			
+			c_markov = Counter(markov)
+			df = pd.DataFrame.from_dict(d,orient='index').reset_index()
+			writting_date(df,str(index))
+			base_day = day
+			markov = []	
+
+def writting_date(index,name):
+	index.to_csv(name)
+
+if __name__ == '__main__':
+	ginza["day"] = convert_date(ginza["day"])
+	print(ginza["day"])
