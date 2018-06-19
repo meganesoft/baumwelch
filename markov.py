4830d9664371b81f86c4115965943afd9497e617
otsuka <meganesoft87@gmai.com>
2018-06-19 20:53:10 +0900

    dask

--- markov.py
+++ markov.py
@@ -1,9 +1,10 @@
 import re
 import pandas as pd
 import datetime as dt
-from collections import Counter
+import  collections as cl
 import os
 from tqdm import tqdm
+import dask.dataframe as dd
 
 ginza = pd.read_csv("trance_ginza.csv")
 ginza.columns = ["index","day","post","devies","etc","etc2","etc3","etc4","etc5"]
@@ -29,26 +30,28 @@ def comparison_date(column,interval):
 	base_day = column[:1]
 	markov = []
 	for (index,day) in tqdm(column.iterrows()):
-		print(base_day)
-		if base_day["devies"].item() == day["devies"]:
-			if day["day"] - pd.to_datetime(base_day["day"].item()) <= dt.timedelta(days=interval):
+		if pd.Series(base_day["devies"]).item() == day["devies"]:
+			if day["day"] - pd.to_datetime(pd.Series(base_day["day"]).item()) <= dt.timedelta(days=interval):
 				markov.append(day.post)
 		else:
 			#やっぱこっちに書く
-			c_markov = Counter(markov)
-			print(c_markov)
+			c_markov = cl.Counter(markov)
+			#print(c_markov)
 			#df = pd.DataFrame.from_dict(c_markov,orient='columns').reset_index()
 			#writting_date(df,str(index))
 			base_day = day
-			markov = []	
-
+			markov = []
+			
+	print("完了")
 def writting_date(index,name):
-	index.to_csv(name)
+	index.to_csv("test.csv")
+
 
 if __name__ == '__main__':
 	print(type(trance_ginza))
 	trance_ginza["day"] = pd.Series(convert_date(ginza["day"]).values.flatten())
 	trance_ginza["post"] = ginza["post"]
 	trance_ginza["devies"] = ginza["devies"]
-	#intervalは時間の間(1時間単位)	
+	#intervalは時間の間(1時間単位)
+	print("ではやっていきます")	
 	comparison_date(trance_ginza,interval)
